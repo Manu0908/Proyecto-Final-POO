5a4c37a95de5d5127d62f22755e8a8ac23775ca8
Manu0908 <72828055+Manu0908@users.noreply.github.com>
2026-07-18 22:52:42 -0500

    chore: setup initial C# project boilerplate

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Final_POO_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
